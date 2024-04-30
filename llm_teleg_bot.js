7a6d218766237511312d3651faf74b2adbe60446
erebodino <erebodino@gmail.com>
2024-04-30 20:25:20 -0300

    add new command

--- llm_teleg_bot.js
+++ llm_teleg_bot.js
@@ -10,20 +10,45 @@ const bot = new TelegramBot(token, { polling: true });
 
 //fast_bot endpoint
 const apiUrlBase = process.env.API_URL_BASE || 'http://127.0.0.1:8000';
-const apiUrl = `${apiUrlBase}/api/v1/message`;
+const apiMessageUrl = `${apiUrlBase}/message/`;
+const apiRegisterUrl = `${apiUrlBase}/register_user/`;
 
 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/,(msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, '¡Hola! Soy un bot de ejemplo. ¿Cómo puedo ayudarte?');
+    bot.sendMessage(chatId, "Hi, I'm a bot to register expenses, send me a message with a expense and I will analize it.");
+});
+
+
+bot.onText(/\/register_me/, async(msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'I will register you, please wait a moment.');
+    try {
+        const response = await axios.post(apiRegisterUrl, {
+            telegram_id: chatId.toString(),
+        });
+
+        if (response.data.message) {
+            const message = response.data.message;
+            bot.sendMessage(chatId, message);
+        }
+    } catch (error) {
+        console.error('Error al enviar el mensaje a la API:', error);
+    }
+
 });
 
 bot.on('message', async (msg) => {
+
     const chatId = msg.chat.id;
     const messageText = msg.text;
+    
+    if (messageText.startsWith('/')) {
+        return;
+    }
 
     try {
-        const response = await axios.post(apiUrl, {
+        const response = await axios.post(apiMessageUrl, {
             chatId: chatId,
             messageText: messageText
         });
